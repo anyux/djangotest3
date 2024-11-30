2c653f08e84681bde3c830b23ff89d31bab6dfdc
anyu <1915530614@qq.com>
2024-11-30 14:42:42 +0800

    02 序列化

--- app/models.py
+++ app/models.py
@@ -19,3 +19,34 @@ class UserInfo(models.Model):
         # 复数形式也显示为单数
         verbose_name_plural = verbose_name
 
+
+class Addr(models.Model):
+    """地址信息模型类"""
+    user = models.ForeignKey('UserInfo',verbose_name="所属用户",on_delete=models.CASCADE)
+    mobile = models.CharField(verbose_name="手机号",max_length=18)
+    city = models.CharField(verbose_name="城市",max_length=10)
+    info = models.CharField(verbose_name="详细地址",max_length=200)
+
+    def __str__(self):
+        return self.info
+
+    class Meta:
+        db_table = "addr"
+        verbose_name = "地址表"
+        verbose_name_plural = verbose_name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/serializers.py
+++ app/serializers.py
@@ -1,11 +1,27 @@
 from rest_framework import serializers
 
+from app.models import UserInfo
 
 
 class UserInfoSerializer(serializers.Serializer):
     """定义序列化器"""
     name = serializers.CharField(max_length=20)
-    id = serializers.IntegerField()
+    id = serializers.IntegerField(read_only=True)
     pwd = serializers.CharField(max_length=20)
     email = serializers.EmailField(max_length=40)
     age = serializers.IntegerField(min_value=0, max_value=150)
+
+class AddressSerializer(serializers.Serializer):
+    """定义地址表序列化器"""
+    id = serializers.IntegerField(read_only=True)
+    mobile = serializers.CharField(max_length=18)
+    city = serializers.CharField(max_length=10)
+    info = serializers.CharField(max_length=200)
+    # 返回关联模型对象的主键,此处必须指定关联到的对象queryset或者指定参数 read_only=True
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
+    # 返回整个外键关联的信息
+    # user = UserInfoSerializer()
+    # 返回该模型的__str__返回值
+    # user = serializers.StringRelatedField()
+    # 返回自定义的关联的外键字段,只能指定一个字段
+    # user = serializers.SlugRelatedField(read_only=True,slug_field='email')

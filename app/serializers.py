d4b7dc2bd5656236fa5b8dd0c429060161027bb1
anyu <1915530614@qq.com>
2024-11-30 23:18:36 +0800

    03 模型序列化器

--- app/serializers.py
+++ app/serializers.py
@@ -1,42 +1,35 @@
 from rest_framework import serializers
 
-from app.models import UserInfo
+from app.models import UserInfo, Addr
 
 
-class UserInfoSerializer(serializers.Serializer):
-    """定义序列化器"""
-    name = serializers.CharField(max_length=20)
-    id = serializers.IntegerField(read_only=True)
-    pwd = serializers.CharField(max_length=20)
-    email = serializers.EmailField(max_length=40)
-    age = serializers.IntegerField(min_value=0, max_value=150)
+class UserInfoSerializer(serializers.ModelSerializer):
+    """定义用户序列化器"""
 
-    def create(self, validated_data):
-        """自定义序列化器保存数据的方法"""
+    class Meta:
+        model = UserInfo
+        # 所有字段
+        fields = '__all__'
+        # 指定序列化字段
+        # fields = ('id','age','pwd')
+        # 字段id不参与序列化,注意,此字段值必须为 list or tuple
+        # exclude = ('id',)
+        # read_only_fields = ("id",)
+        # 使用extra_kwargs 去修改指定字段的校验规则(反序列化的校验规则)
+        extra_kwargs = {
+            'pwd': {
+                'required': True,
+                'min_length': 8,
+                'max_length': 20,
+                'error_messages': {
+                    ''
+                }
+            }
+        }
 
-        return UserInfo.objects.create(**validated_data)
+class AddrSerializer(serializers.ModelSerializer):
+    """定义地址序列化器"""
 
-    def update(self, instance, validated_data):
-        """自定义序列化器更新数据的方法"""
-        type(instance)
-        instance.name = validated_data.get('name', instance.name)
-        instance.pwd = validated_data.get('pwd', instance.pwd)
-        instance.email = validated_data.get('email', instance.email)
-        instance.age = validated_data.get('age', instance.age)
-        instance.save()
-        return instance
-
-class AddressSerializer(serializers.Serializer):
-    """定义地址表序列化器"""
-    id = serializers.IntegerField(read_only=True)
-    mobile = serializers.CharField(max_length=18)
-    city = serializers.CharField(max_length=10)
-    info = serializers.CharField(max_length=200)
-    # 返回关联模型对象的主键,此处必须指定关联到的对象queryset或者指定参数 read_only=True
-    user = serializers.PrimaryKeyRelatedField(read_only=True)
-    # 返回整个外键关联的信息
-    # user = UserInfoSerializer()
-    # 返回该模型的__str__返回值
-    # user = serializers.StringRelatedField()
-    # 返回自定义的关联的外键字段,只能指定一个字段
-    # user = serializers.SlugRelatedField(read_only=True,slug_field='email')
+    class Meta:
+        model = Addr
+        fields = '__all__'

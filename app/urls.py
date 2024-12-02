91df90c0ced528b76f6a8b2b3650b5d580c821e1
gaochao <1915530614@qq.com>
2024-12-02 18:31:18 +0800

    04 视图函数

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,12 @@
+
 from django.urls import path, include, re_path
 
-from app.views import user_list, user_detail
+from app.views import UserApiView, UserDetailApiView
 
 urlpatterns = [
     # 列表url
-    re_path(r'^users/?$',user_list,name='user_list'),
-    # 单个节点url
-    re_path(r'^users/?(?P<id>\d+)/?$',user_detail,name='user_detail'),
+    re_path(r'^users/?$', UserApiView.as_view()),
+    re_path(r'^users/(?P<id>\d+)/?$', UserDetailApiView.as_view()),
+
 ]
+
--- app/views.py
+++ app/views.py
@@ -1,21 +1,14 @@
-from django.http import JsonResponse
-from django.shortcuts import render
-from rest_framework.parsers import JSONParser
+from django.http import Http404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from app.models import UserInfo
 from app.serializers import UserInfoSerializer
 
 
-# Create your views here.
-
-def user_list(request):
-    """
-    get方法请求: 获取用户列表
-    post方法请求: 添加用户信息
-    :param request:
-    :return:
-    """
-    if request.method == 'GET':
+class UserApiView(APIView):
+    def get(self, request):
         #获取用户列表并返回
         user = UserInfo.objects.all()
         user_serializer = UserInfoSerializer(user, many=True)
@@ -24,61 +17,58 @@ def user_list(request):
             "code": 200,
             "msg": "success",
         }
-        return JsonResponse(result)
-    elif request.method == 'POST':
-        #添加用户信息
-        params = JSONParser().parse(request)
+        return Response(result,status=status.HTTP_200_OK)
+    def post(self, request):
         #创建序列化器
-        serializer = UserInfoSerializer(data=params)
+        serializer = UserInfoSerializer(data=request.data)
         #检查序列化器
         if serializer.is_valid():
             #检查通过,提交数据
             serializer.save()
             # 后端序列化器,将id设置为只读,即反序列化时,不会将id字段写入到数据库,而是让它自动生成
-            return JsonResponse({"code":201,"data":serializer.data,"message":"ok"}, status=201)
+            return Response({"code":201,"data":serializer.data,"message":"ok"}, status=status.HTTP_201_CREATED)
         else:
             #检查失败,提示失败信息
-            return JsonResponse(serializer.errors, status=400)
-    #请求方法不对,返回方法问题
-    return JsonResponse({"code":400,"message":f"当前地址不支持此请求方法: {request.method}"}, status=201)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-def user_detail(request,id):
-    """
-    get方法请求: 获取用户信息
-    delete方法请求: 删除用户信息
-    put: 修改用户信息
-    :param request:
-    :return:
-    """
-    try:
-        # 获取url传参数id
-        user = UserInfo.objects.get(id=id)
-    except UserInfo.DoesNotExist:
-        return JsonResponse({"code":404,"message":f"未发现: {id}"}, status=404)
-    if request.method == 'GET':
-        user_serializer = UserInfoSerializer(user)
+
+
+
+
+class UserDetailApiView(APIView):
+
+    def get_object(self,id):
+        try:
+            # 获取url传参数id
+            user = UserInfo.objects.get(id=id)
+            return user
+        except UserInfo.DoesNotExist:
+            # 抛出异常
+            raise Http404
+
+    def get(self,request,id,format=None):
+        user = self.get_object(id=id)
+        user_serializer = UserInfoSerializer(instance=user)
         result ={
-            "data": user_serializer.data,
-            "code": 200,
-            "msg": "success",
+        "data": user_serializer.data,
+        "code": 200,
+        "msg": "success",
         }
-        return JsonResponse(result)
-    elif request.method == 'DELETE':
-        #删除资源
-        user.delete()
-        # 删除资源返回空对象,状态为204
-        return JsonResponse({},status=204)
-    elif request.method == 'PUT':
-        #修改资源
-        params = JSONParser().parse(request)
-        serializer = UserInfoSerializer(instance=user, data=params)
+        return Response(result)
+
+    def put(self, request, id):
+        user = self.get_object(id=id)
+
+        serializer = UserInfoSerializer(instance=user, data=request.data)
         if serializer.is_valid():
             serializer.save()
             # 后端序列化器,将id设置为只读,即反序列化时,不会将id字段写入到数据库,而是让它自动生成
-            return JsonResponse({"code":201,"data":serializer.data,"message":"ok"}, status=201)
+            return Response({"code":201,"data":serializer.data,"message":"ok"}, status=status.HTTP_200_OK)
         else:
-            return JsonResponse({"code":400,"message":serializer.errors}, status=400)
+            return Response({"code":400,"message":serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
-    else:
-        #请求方法不对,返回方法问题
-        return JsonResponse({"code":400,"message":f"当前地址不支持此请求方法: {request.method}"}, status=201)
+    def delete(self, request, id):
+        #删除资源
+        self.get_object(id=id).delete()
+        # 删除资源返回空对象,状态为204
+        return Response({},status=status.HTTP_204_NO_CONTENT)

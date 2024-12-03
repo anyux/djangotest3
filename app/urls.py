0ea5e10552a322426fdb68fa3766e7fb5efadee7
gaochao <1915530614@qq.com>
2024-12-03 17:39:27 +0800

    05 复杂视图函数 apiViewSet视图集类

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,16 @@
 
 from django.urls import path, include, re_path
 
-from app.views import UserApiView, UserDetailApiView, AddrApiView, AddrDetailApiView, UserGenericApiView, UserDetailGenericApiView
+from app.views import UserApiView, UserDetailApiView, AddrApiView, AddrDetailApiView, UserGenericApiView, UserDetailGenericApiView, \
+    UserGenericApiViewMixin, UserGenericDetailApiViewMixin, UserGenericApiViewMultiples, UserGenericDetailApiViewMultiples, \
+    UserViewSet
 
 urlpatterns = [
+    re_path(r'^users/?$', UserViewSet.as_view({"get":"list","post":"create"}), name='user'),
+    re_path(r'^users/(?P<id>\d+)/?$', UserViewSet.as_view({"get":"retrieve","put":"update","delete":"destroy"}), name='user'),
     # 列表url
-    re_path(r'^users/?$', UserGenericApiView.as_view()),
-    re_path(r'^users/(?P<id>\d+)/?$', UserDetailGenericApiView.as_view()),
+    # re_path(r'^users/?$', UserGenericApiViewMultiples.as_view()),
+    # re_path(r'^users/(?P<id>\d+)/?$', UserGenericDetailApiViewMultiples.as_view()),
 
     re_path(r'^addrs/?$', AddrApiView.as_view()),
     re_path(r'^addrs/(?P<id>\d+)/?$', AddrDetailApiView.as_view()),
